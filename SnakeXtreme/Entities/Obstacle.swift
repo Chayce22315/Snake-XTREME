a8d275bb704a94cd3a420dd4b9a778d3b9cbadcd
Chayce22315 <chayceray@outlook.com>
2026-03-26 16:54:16 -0500

    Update Obstacle.swift

--- SnakeXtreme/Entities/Obstacle.swift
+++ SnakeXtreme/Entities/Obstacle.swift
@@ -2,9 +2,9 @@ import SpriteKit
 
 class Obstacle {
     var node: SKShapeNode
-    
-    init(position: CGPoint, size: CGSize) {
-        node = SKShapeNode(rectOf: size)
+
+    init(position: CGPoint) {
+        node = SKShapeNode(rectOf: CGSize(width: 30, height: 30))
         node.fillColor = .gray
         node.position = position
     }
