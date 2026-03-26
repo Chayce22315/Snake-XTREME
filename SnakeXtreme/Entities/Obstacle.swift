55c4bebbda77b64476d9c0662032d43aae5b8fd2
Chayce22315 <chayceray@outlook.com>
2026-03-26 13:25:06 -0500

    Update Obstacle.swift

--- SnakeXtreme/Entities/Obstacle.swift
+++ SnakeXtreme/Entities/Obstacle.swift
@@ -1,17 +1,14 @@
 import SpriteKit
-class Obstacle{
- var nodes:[SKShapeNode]=[]
- func spawn(in s:SKScene){
-  for _ in 0..<5{
-   let o=SKShapeNode(rectOf:CGSize(width:30,height:30))
-   o.fillColor = .gray
-   o.position=random(s)
-   s.addChild(o)
-   nodes.append(o)
-  }
- }
- func random(_ s:SKScene)->CGPoint{
-  CGPoint(x:CGFloat.random(in:50...s.frame.width-50),
-          y:CGFloat.random(in:50...s.frame.height-50))
- }
+
+class Obstacle {
+    let node: SKSpriteNode
+
+    init(position: CGPoint, size: CGSize, color: SKColor = .gray) {
+        node = SKSpriteNode(color: color, size: size)
+        node.position = position
+    }
+
+    func addToScene(_ scene: SKScene) {
+        scene.addChild(node)
+    }
 }
