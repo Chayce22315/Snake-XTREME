c25ec44afa3fe3e6c627df2cec6ec8c612f8046a
Chayce22315 <chayceray@outlook.com>
2026-03-26 16:19:03 -0500

    Update GameCenterManager.swift

--- SnakeXtreme/Managers/GameCenterManager.swift
+++ SnakeXtreme/Managers/GameCenterManager.swift
@@ -1,12 +1,18 @@
 import GameKit
-class GameCenterManager{
- static let shared=GameCenterManager()
- func auth(){
-  GKLocalPlayer.local.authenticateHandler={_,_ in}
- }
- func score(_ v:Int){
-  let s=GKScore(leaderboardIdentifier:"snakeXtreme_highscore")
-  s.value=Int64(v)
-  GKScore.report([s],withCompletionHandler:nil)
- }
+
+class GameCenterManager: NSObject, GKGameCenterControllerDelegate {
+    static let shared = GameCenterManager()
+    
+    func authenticateLocalPlayer() {
+        let localPlayer = GKLocalPlayer.local
+        localPlayer.authenticateHandler = { vc, error in
+            if let vc = vc {
+                // present Game Center login
+            }
+        }
+    }
+    
+    func gameCenterViewControllerDidFinish(_ gameCenterViewController: GKGameCenterViewController) {
+        gameCenterViewController.dismiss(animated: true)
+    }
 }
