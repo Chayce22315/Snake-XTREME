5e78f104e478fc71a8b9c1d26ca6ee1a07d0ad4c
Chayce22315 <chayceray@outlook.com>
2026-03-26 13:09:43 -0500

    Update MainMenuScene.swift

--- SnakeXtreme/Scenes/MainMenuScene.swift
+++ SnakeXtreme/Scenes/MainMenuScene.swift
@@ -1,14 +1,48 @@
 import SpriteKit
-class MainMenuScene:SKScene{
- override func didMove(to view:SKView){
-  backgroundColor=.black
-  let playBtn=SKLabelNode(text:"PLAY");playBtn.position=CGPoint(x:frame.midX,y:frame.midY+50);playBtn.name="play";playBtn.fontSize=40;addChild(playBtn)
-  let shopBtn=SKLabelNode(text:"SHOP");shopBtn.position=CGPoint(x:frame.midX,y:frame.midY-50);shopBtn.name="shop";shopBtn.fontSize=40;addChild(shopBtn)
- }
- override func touchesEnded(_ touches:Set<UITouch>,with event:UIEvent?){
-  guard let t=touches.first else{return}
-  let n=atPoint(t.location(in:self))
-  if n.name=="play"{let scene=GameScene(size:self.size);scene.scaleMode=.resizeFill;view?.presentScene(scene)}
-  else if n.name=="shop"{let scene=ShopScene(size:self.size);scene.scaleMode=.resizeFill;view?.presentScene(scene)}
- }
+
+class MainMenuScene: SKScene {
+
+    override func didMove(to view: SKView) {
+        backgroundColor = .black
+
+        let title = SKLabelNode(text: "SNAKEXTREME")
+        title.fontName = "Chalkduster"
+        title.fontSize = 120
+        title.fontColor = .green
+        title.position = CGPoint(x: size.width/2, y: size.height*0.7)
+        addChild(title)
+
+        let playButton = SKLabelNode(text: "PLAY")
+        playButton.fontName = "Chalkduster"
+        playButton.fontSize = 100
+        playButton.fontColor = .white
+        playButton.position = CGPoint(x: size.width/2, y: size.height*0.5)
+        playButton.name = "playButton"
+        addChild(playButton)
+
+        let shopButton = SKLabelNode(text: "SHOP")
+        shopButton.fontName = "Chalkduster"
+        shopButton.fontSize = 100
+        shopButton.fontColor = .yellow
+        shopButton.position = CGPoint(x: size.width/2, y: size.height*0.35)
+        shopButton.name = "shopButton"
+        addChild(shopButton)
+    }
+
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        guard let touch = touches.first else { return }
+        let location = touch.location(in: self)
+        let nodesAtPoint = nodes(at: location)
+        for node in nodesAtPoint {
+            if node.name == "playButton" {
+                let scene = GameScene(size: size)
+                scene.scaleMode = .aspectFill
+                view?.presentScene(scene, transition: .doorsOpenHorizontal(withDuration: 0.5))
+            } else if node.name == "shopButton" {
+                let scene = ShopScene(size: size)
+                scene.scaleMode = .aspectFill
+                view?.presentScene(scene, transition: .doorsOpenVertical(withDuration: 0.5))
+            }
+        }
+    }
 }
