a9058fbe17ff75dd3350620827e4152f52fd77ed
Chayce22315 <chayceray@outlook.com>
2026-03-26 16:20:48 -0500

    Update MainMenuScene.swift

--- SnakeXtreme/Scenes/MainMenuScene.swift
+++ SnakeXtreme/Scenes/MainMenuScene.swift
@@ -4,14 +4,16 @@ class MainMenuScene: SKScene {
 
     override func didMove(to view: SKView) {
         backgroundColor = .black
-
+        
+        // MARK: - Title
         let title = SKLabelNode(text: "SNAKEXTREME")
         title.fontName = "Chalkduster"
         title.fontSize = 120
         title.fontColor = .green
         title.position = CGPoint(x: size.width/2, y: size.height*0.7)
         addChild(title)
-
+        
+        // MARK: - Play Button
         let playButton = SKLabelNode(text: "PLAY")
         playButton.fontName = "Chalkduster"
         playButton.fontSize = 100
@@ -19,7 +21,8 @@ class MainMenuScene: SKScene {
         playButton.position = CGPoint(x: size.width/2, y: size.height*0.5)
         playButton.name = "playButton"
         addChild(playButton)
-
+        
+        // MARK: - Shop Button
         let shopButton = SKLabelNode(text: "SHOP")
         shopButton.fontName = "Chalkduster"
         shopButton.fontSize = 100
@@ -29,19 +32,24 @@ class MainMenuScene: SKScene {
         addChild(shopButton)
     }
 
+    // MARK: - Touch Handling
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
         guard let touch = touches.first else { return }
         let location = touch.location(in: self)
         let nodesAtPoint = nodes(at: location)
+        
         for node in nodesAtPoint {
-            if node.name == "playButton" {
+            switch node.name {
+            case "playButton":
                 let scene = GameScene(size: size)
                 scene.scaleMode = .aspectFill
                 view?.presentScene(scene, transition: .doorsOpenHorizontal(withDuration: 0.5))
-            } else if node.name == "shopButton" {
+            case "shopButton":
                 let scene = ShopScene(size: size)
                 scene.scaleMode = .aspectFill
                 view?.presentScene(scene, transition: .doorsOpenVertical(withDuration: 0.5))
+            default:
+                break
             }
         }
     }
