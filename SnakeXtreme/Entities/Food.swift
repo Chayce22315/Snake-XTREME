e1423d64c969adb5d6aa3b287d39bea598b9e4e6
Chayce22315 <chayceray@outlook.com>
2026-03-26 12:58:22 -0500

    Refactor Food class to inherit from SKShapeNode
    Updated Food class to inherit from SKShapeNode and added initializer for position.

--- SnakeXtreme/Entities/Food.swift
+++ SnakeXtreme/Entities/Food.swift
@@ -1,14 +1,16 @@
 import SpriteKit
-class Food{
- var node:SKShapeNode!
- func spawn(in s:SKScene){
-  node=SKShapeNode(circleOfRadius:10)
-  node.fillColor=.red
-  node.position=random(s)
-  s.addChild(node)
- }
- func random(_ s:SKScene)->CGPoint{
-  CGPoint(x:CGFloat.random(in:50...s.frame.width-50),
-          y:CGFloat.random(in:50...s.frame.height-50))
- }
+
+class Food: SKShapeNode {
+    static let blockSize: CGFloat = 64
+
+    init(position: CGPoint) {
+        super.init()
+        self.position = position
+        self.path = CGPath(rect: CGRect(x: 0, y: 0, width: Food.blockSize, height: Food.blockSize), transform: nil)
+        self.fillColor = .red
+        self.strokeColor = .clear
+        self.zPosition = 1
+    }
+
+    required init?(coder aDecoder: NSCoder) { fatalError("init(coder:) not implemented") }
 }
