adeade8391cb52ae0f62b58df33cf04f3916488a
Chayce22315 <chayceray@outlook.com>
2026-03-26 16:15:37 -0500

    Update GameManager.swift

--- SnakeXtreme/Game/GameManager.swift
+++ SnakeXtreme/Game/GameManager.swift
@@ -1,18 +1,22 @@
-class GameManager{
- static let shared = GameManager()
- var score = 0
- var coins = 0
- var ownedTrails: [String] = ["default"]
- var equippedTrail: String = "default"
- func addCoins(_ c:Int){coins+=c}
- func buyTrail(id:String,cost:Int)->Bool{
-  if coins>=cost && !ownedTrails.contains(id){
-   coins-=cost
-   ownedTrails.append(id)
-   return true
-  }
-  return false
- }
- func equipTrail(id:String){if ownedTrails.contains(id){equippedTrail=id}}
- func reset(){score=0}
+import SpriteKit
+
+class GameManager {
+    var snake: Snake
+    var food: Food
+    var obstacles: [Obstacle] = []
+    
+    init(snake: Snake, food: Food) {
+        self.snake = snake
+        self.food = food
+    }
+    
+    func update() {
+        checkFoodCollision()
+    }
+    
+    private func checkFoodCollision() {
+        if snake.head.frame.intersects(food.node.frame) {
+            // handle food eaten
+        }
+    }
 }
