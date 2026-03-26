834e474de5ad5055b05ea97c35dadcd77a252654
Chayce22315 <chayceray@outlook.com>
2026-03-26 17:05:06 -0500

    Update Food.swift

--- SnakeXtreme/Entities/Food.swift
+++ SnakeXtreme/Entities/Food.swift
@@ -6,13 +6,5 @@ class Food {
     init() {
         node = SKShapeNode(circleOfRadius: 10)
         node.fillColor = .red
-        node.position = CGPoint(x: 200, y: 200)
-    }
-
-    func respawn() {
-        node.position = CGPoint(
-            x: CGFloat.random(in: 20...380),
-            y: CGFloat.random(in: 20...780)
-        )
     }
 }
