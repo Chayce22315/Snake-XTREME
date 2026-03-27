bc263e4e258420d4dc46d3f49e8a226fa7b62525
Chayce22315 <chayceray@outlook.com>
2026-03-26 22:25:38 -0500

    Update AppDelegate.swift

--- SnakeXtreme/AppDelegate.swift
+++ SnakeXtreme/AppDelegate.swift
@@ -1,3 +1,25 @@
 import UIKit
+import SwiftUI
+
 @main
-class AppDelegate:UIResponder,UIApplicationDelegate{}
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    // MARK: - UIApplicationDelegate
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        // create window manually since we might not have a SceneDelegate
+        let window = UIWindow(frame: UIScreen.main.bounds)
+
+        // your main game view controller
+        let rootViewController = GameViewController()
+        window.rootViewController = rootViewController
+        self.window = window
+        window.makeKeyAndVisible()
+
+        return true
+    }
+}
